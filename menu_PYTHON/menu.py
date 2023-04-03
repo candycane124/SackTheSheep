08795b9270805acf61254265b2517dc7a1d62498
BambooShi <110565810+BambooShi@users.noreply.github.com>
2023-04-03 10:42:16 -0400

    updated menu, can now add images (220x60)

--- menu_PYTHON/menu.py
+++ menu_PYTHON/menu.py
@@ -5,31 +5,12 @@ from PIL import ImageTk, Image
 
 root = tk.Tk()
 root.geometry('500x500')
-# #%% Frames
+
 frameA = tk.Frame(width=500, height = 150, background="white")
 frameB = tk.Frame(width=500, height = 350, background="green")
-# Nested Frame. framebb is created within frameB without width or height
-# framebb = tk.Frame(frameB, background="green")
-
-# title = ImageTk.PhotoImage( Image.open('menu_PYTHON/menuAssets/title.png'), Image.ANTIALIAS)
-# labelTitle = tk.Label(root, image=title)
-# labelTitle.place(relx=0.5, rely=0.15, anchor='center')
-
-# img = ImageTk.PhotoImage(Image.open('menu_PYTHON/menuAssets/grass-588.jpg'), Image.ANTIALIAS)
-# lbl = tk.Label(root, image = img)
-# lbl.img = img  # Keep a reference in case this code put is in a function.
-# lbl.place(relx=0.5, rely=0.5, anchor='center')  # Place label in center of parent.
-
-# # Add other tkinter widgets.
-# button = tk.Button(root, text="Start")
-# button.grid(row=0, column=0)
-# button = tk.Button(root, text="Continue")
-# button.grid(row=0, column=1, padx=10)
 
 frameA.pack(side="top")
 frameB.pack(side='top')
-# expand is the key parameter to center the framebb within frameB
-# framebb.pack(expand=True)
 
 frameA.pack_propagate(False)
 frameB.pack_propagate(False)
@@ -44,8 +25,11 @@ img = ImageTk.PhotoImage(Image.open('menu_PYTHON/menuAssets/grass-588.jpg'), Ima
 lbl = tk.Label(frameB, image=img)
 lbl.place(relx=0.5, rely=0.5,anchor='center')  # Place label in center of parent.
 
-btnStart = tk.Button(frameB, text='PLAY', width = '30', height='3', bd='3').pack(side="top", pady=(50,10)) #needs to bring user to the next screen; levels or level 1
-btnSetting = tk.Button(frameB, text = 'SETTINGS', width = '30', height='3',bd='3').pack(side="top", pady=10)
-btnExit = tk.Button(frameB, text = 'EXIT', width = '30', height='3',bd='3', command = root.destroy).pack(side="top", pady=10)
+# Add Image
+login_btn = tk.PhotoImage(file = "menu_PYTHON/menuAssets/test_red.png")
+
+btnStart = tk.Button(frameB, image=login_btn, borderwidth=0).pack(side="top", pady=(50,10)) #needs to bring user to the next screen; levels or level 1
+btnSetting = tk.Button(frameB, image=login_btn, borderwidth=0).pack(side="top", pady=10)
+btnExit = tk.Button(frameB, image=login_btn, borderwidth=0, command = root.destroy).pack(side="top", pady=10)
 
 root.mainloop()
