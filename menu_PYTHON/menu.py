de3905770ddf651a0ab038ba089ad955a1a2bd2d
BambooShi <110565810+BambooShi@users.noreply.github.com>
2023-04-05 16:05:14 -0400

    Added images for the buttons + changed title img

--- menu_PYTHON/menu.py
+++ menu_PYTHON/menu.py
@@ -1,13 +1,15 @@
 # menu to the game Sack The Sheep
 import tkinter as tk
+from tkinter import ttk
 # loading Python Imaging Library
 from PIL import ImageTk, Image
 
 root = tk.Tk()
 root.geometry('500x500')
+style = ttk.Style()
 
-frameA = tk.Frame(width=500, height = 150, background="white")
-frameB = tk.Frame(width=500, height = 350, background="green")
+frameA = tk.Frame(width=500, height = 150, background="light blue")
+frameB = tk.Frame(width=500, height = 350)
 
 frameA.pack(side="top")
 frameB.pack(side='top')
@@ -15,11 +17,9 @@ frameB.pack(side='top')
 frameA.pack_propagate(False)
 frameB.pack_propagate(False)
 
-title = ImageTk.PhotoImage( Image.open('menu_PYTHON/menuAssets/title.png'), Image.ANTIALIAS)
+title = ImageTk.PhotoImage( Image.open('menu_PYTHON/menuAssets/titlePot.png'), Image.ANTIALIAS)
 labelTitle = tk.Label(frameA, image=title)
 labelTitle.place(relx=0.5, rely=0.5, anchor='center')
-
-startImg = tk.PhotoImage(file = 'menu_PYTHON/menuAssets/title.png')
 
 img = ImageTk.PhotoImage(Image.open('menu_PYTHON/menuAssets/grass-588.jpg'), Image.ANTIALIAS)
 lbl = tk.Label(frameB, image=img)
@@ -27,9 +27,20 @@ lbl.place(relx=0.5, rely=0.5,anchor='center')  # Place label in center of parent
 
 # Add Image
 login_btn = tk.PhotoImage(file = "menu_PYTHON/menuAssets/test_red.png")
+start_btn = tk.PhotoImage(file = "menu_PYTHON/menuAssets/potentialStart.png")
+settings_btn = tk.PhotoImage(file = "menu_PYTHON/menuAssets/settingsPot.png")
+exit_btn = tk.PhotoImage(file = "menu_PYTHON/menuAssets/exitPot.png")
 
-btnStart = tk.Button(frameB, image=login_btn, borderwidth=0).pack(side="top", pady=(50,10)) #needs to bring user to the next screen; levels or level 1
-btnSetting = tk.Button(frameB, image=login_btn, borderwidth=0).pack(side="top", pady=10)
-btnExit = tk.Button(frameB, image=login_btn, borderwidth=0, command = root.destroy).pack(side="top", pady=10)
+btnStart = ttk.Button(frameB, image=start_btn) #needs to bring user to the next screen; levels or level 1
+btnSetting = ttk.Button(frameB, image=settings_btn) #settings
+btnExit = ttk.Button(frameB, image=exit_btn,  command = root.destroy) #exit
+
+style.theme_use('alt')
+style.configure('TButton', background='#232323', foreground='white')
+style.map('TButton', background=[('active', '#008000')])
+btnStart.pack(side="top", pady=(50,10))
+btnSetting.pack(side="top", pady=10)
+btnExit.pack(side="top", pady=10)
+
 
 root.mainloop()
