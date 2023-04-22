189a6a620b8561ff7774c3e33db296493b0d8184
BambooShi <110565810+BambooShi@users.noreply.github.com>
2023-04-22 14:20:16 -0400

    Changed level select image, disabled buttons for levels page

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from tkinter import font as tkfont
 # loading Python Imaging Library
 from PIL import ImageTk, Image
 from tkinter import ttk
+from tkinter import font as tkFont
 
 class SampleApp(tk.Tk):
 
@@ -54,7 +55,7 @@ class Shop(tk.Frame):
         label = tk.Label(self, text="This is the store page", font=controller.title_font)
         label.pack(side="top", fill="x", pady=10)
 
-        button1 = tk.Button(self, text="Back", cursor="target",
+        button1 = tk.Button(self, text="Back", cursor="sb_left_arrow",
                             command=lambda: controller.show_frame("Menu"))
 
         button1.pack()
@@ -114,6 +115,19 @@ class LevelSelect(tk.Frame):
         tk.Frame.__init__(self, parent)
         self.controller = controller
 
+        levelComplete1 = False #status of level 1 completion
+        levelComplete2 = False #status of level 2 completion
+
+        helv36 = tkFont.Font(family='Helvetica', size=18, weight='bold')
+
+        #Background
+        img = ImageTk.PhotoImage(Image.open('assets/grass-588.jpg'))
+        lbl = tk.Label(self, image=img)
+        
+        lbl.image = img #keeping a reference, so the image will appear properly
+        # lbl.pack()
+        lbl.place(relx=0.5, rely=0.5, anchor="center")  # Place label in center of parent.
+
         #Title image
         title = ImageTk.PhotoImage(Image.open('menu_PYTHON/menuAssets/LevelTemp.png'))
         labelTitle = tk.Label(self, image=title)
@@ -122,22 +136,39 @@ class LevelSelect(tk.Frame):
         # labelTitle.pack()
         labelTitle.place(bordermode="inside", anchor="n", relx=0.5)
 
-        button = tk.Button(self, text="Back", cursor="target",
+        button = tk.Button(self, text="Back", cursor="sb_left_arrow",
                            command=lambda: controller.show_frame("Menu"))
         button.pack(side="bottom")
 
-        btn1 = tk.Button(self, text="Level 1", cursor="target", ) #bring user to level 1
-        btn2 = tk.Button(self, text="Level 2", cursor="target") #bring user to level 2; should be locked
-        btn3 = tk.Button(self, text="Level 3",  cursor="target") #bring user to level 3; should be locked
+        btn1 = tk.Button(self, text="Level 1", cursor="target", font=helv36) #bring user to level 1
+        btn2 = tk.Button(self, text="Level 2", cursor="target", font=helv36) #bring user to level 2; should be locked
+        btn3 = tk.Button(self, text="Level 3", cursor="target", font=helv36) #bring user to level 3; should be locked
 
         btn1.place(relx=0.25, rely=0.6, anchor="center")
         btn2.place(relx=0.5, rely=0.6, anchor="center")
         btn3.place(relx=0.75, rely=0.6, anchor="center")
 
         controller.changeOnHover(btn1, "green", "white")
-        controller.changeOnHover(btn2, "green", "white")
-        controller.changeOnHover(btn3, "green", "white")
         controller.changeOnHover(button, "green", "white")
+
+        if levelComplete1 != True:
+            self.lock(btn2)
+            self.lock(btn3)
+        elif levelComplete2 != True:
+            self.lock(btn3)
+            self.unlock(btn2)
+        else:
+            self.unlock(btn3)
+    
+    def lock(self, btn):
+        btn["state"] = "disabled"
+        return
+    
+    def unlock(self, btn):
+        btn["state"] = "normal"
+        self.controller.changeOnHover(btn, "green", "white")
+
+        return
 
 
 if __name__ == "__main__":
